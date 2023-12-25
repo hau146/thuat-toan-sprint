e2c3686ce3ca559183ea395291d1e6c68a6cff06
hau146 <ishimaru274@gmail.com>
2023-12-25 14:48:44 +0700

    hauNH

---
+++ Animal.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class X {
+    public void printFileContent() throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        String a = null;
+        String b = null;
+        String c = a.concat(b);
+        System.out.println(c);
+    }
+}
